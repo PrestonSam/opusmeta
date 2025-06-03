bbae7d3fd1e63b8bdf4687489d1e3578030f64d7
Sam Preston <31986848+PrestonSam@users.noreply.github.com>
2025-06-03 20:18:55 +0100

    Tag struct: Exchanged `String` key for `LowercaseString` (#2)
    * Added LowercaseString as parameters for Tag struct
    
    * Made `get`, `get_one` and `remove_entries` use AsRef<K>.
    
    * Fix clippy suggestions
    
    * Make LowercaseString's inner string pub(crate) and impl Deref
    
    * Add docs to LowercaseString
    
    * Make utils module private
    
    * impl Display for LowercaseString
    
    ---------
    
    Co-authored-by: Yash Karandikar <nerdstep710@gmail.com>

---
+++ src/utils.rs
@@ -0,0 +1,38 @@
+use std::fmt::Display;
+use std::ops::Deref;
+
+/// A lowercase String. Holds a [`String`] internally.
+#[derive(Debug, Clone)]
+pub struct LowercaseString(pub(crate) String);
+
+impl Deref for LowercaseString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Display for LowercaseString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self.0, f)
+    }
+}
+
+impl LowercaseString {
+    /// Create a new `LowercaseString`. This will copy the contents of the argument to a newly
+    /// allocated buffer.
+    #[must_use]
+    pub fn new(str: &str) -> Self {
+        Self(str.to_ascii_lowercase())
+    }
+}
+
+impl<T> From<T> for LowercaseString
+where
+    T: AsRef<str>,
+{
+    fn from(value: T) -> Self {
+        Self::new(value.as_ref())
+    }
+}
