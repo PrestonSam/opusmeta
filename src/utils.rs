6c377bc60548633b11dcaf20fe7aa0ecba2051bc
Yash Karandikar <nerdstep710@gmail.com>
2025-06-03 14:16:57 -0500

    impl Display for LowercaseString

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 use std::ops::Deref;
 
 /// A lowercase String. Holds a [`String`] internally.
@@ -9,6 +10,12 @@ impl Deref for LowercaseString {
 
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+impl Display for LowercaseString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self.0, f)
     }
 }
 
